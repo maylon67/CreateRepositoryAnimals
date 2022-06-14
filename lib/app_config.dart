64f9c49bf1fd45fe53ca4fedf8b12c5b9ea39af3
Maylon <maylongatodemais25@gmail.com>
2022-06-14 16:21:04 -0300

    vigessimo terceiro update

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 final Color corPadraoApp = Color.fromARGB(255, 117, 239, 11);
 final Color corDestackApp = Color.fromARGB(0, 0, 0, 0);
-final Color colorAppBar = Color.fromARGB(255, 117, 239, 11);
+final Color colorAppBar = Color.fromARGB(255, 93, 184, 13);
--- lib/ui/setings_account.dart
+++ lib/ui/setings_account.dart
@@ -3,7 +3,7 @@ import 'package:app_demo/ui/setings_animais.dart';
 import 'package:flutter/material.dart';
 
 class ConfiguracoesConta extends StatefulWidget {
-  const ConfiguracoesConta({ Key? key }) : super(key: key);
+  const ConfiguracoesConta({Key? key}) : super(key: key);
 
   @override
   State<ConfiguracoesConta> createState() => _ConfiguracoesContaState();
@@ -21,6 +21,228 @@ class _ConfiguracoesContaState extends State<ConfiguracoesConta> {
         ),
         backgroundColor: colorScaffold,
         centerTitle: true,
+        shape: UnderlineInputBorder(
+            borderSide: BorderSide(
+          color: colorAppBar,
+        )),  
+      ),
+      backgroundColor: colorScaffold,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Divider(
+              color: corPadraoApp,
+              height: 0,
+              thickness: 1.4,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Editar perfil',
+                    style: TextStyle(
+                      fontSize: 29,
+                      color: corPadraoApp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 76.5),
+                  child: Icon(
+                    Icons.manage_accounts_rounded,
+                    color: corPadraoApp,
+                    size: 49,
+                  ),
+                ),
+              ],
+            ),
+             Divider(
+                  height: 0,
+                  thickness: 2.1,
+                  color: corPadraoApp,
+                ),
+                Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Excluir Conta',
+                    style: TextStyle(
+                      fontSize: 29,
+                      color: corPadraoApp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 48.7),
+                  child: Icon(
+                    Icons.delete_outline_rounded,
+                    color: corPadraoApp,
+                    size: 49,
+                  ),
+                ),
+              ],
+            ), 
+            Divider(
+                  height: 0,
+                  thickness: 2.1,
+                  color: corPadraoApp,
+                ),
+             Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Adiconar Conta',
+                    style: TextStyle(
+                      fontSize: 29,
+                      color: corPadraoApp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 17.7),
+                  child: Icon(
+                    Icons.add_circle_outline_rounded,
+                    color: corPadraoApp,
+                    size: 49,
+                  ),
+                ),
+              ],
+            ), 
+            Divider(
+                  height: 0,
+                  thickness: 2.1,
+                  color: corPadraoApp,
+             ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Privacidade',
+                    style: TextStyle(
+                      fontSize: 29,
+                      color: corPadraoApp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 58.7),
+                  child: Icon(
+                    Icons.private_connectivity_rounded,
+                    color: corPadraoApp,
+                    size: 69,
+                  ),
+                ),
+              ],
+            ),
+             Divider(
+                  height: 0,
+                  thickness: 2.1,
+                  color: corPadraoApp,
+             ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Sincronização',
+                    style: TextStyle(
+                      fontSize: 29,
+                      color: corPadraoApp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 17)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 45.7),
+                     child: Icon(
+                      Icons.archive_rounded,
+                      color: corPadraoApp,
+                      size: 56.2,
+                  ),
+                   ),
+              ],
+            ),
+           Divider(
+                  height: 0,
+                  thickness: 2.1,
+                  color: corPadraoApp,
+             ),
+          Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Fale Conosco',
+                    style: TextStyle(
+                      fontSize: 29,
+                      color: corPadraoApp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 35.7),
+                  child: Icon(
+                    Icons.call_rounded,
+                    color: corPadraoApp,
+                    size: 59,
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+                  height: 0,
+                  thickness: 2.1,
+                  color: corPadraoApp,
+             ),
+           Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Feedback',
+                    style: TextStyle(
+                      fontSize: 29,
+                      color: corPadraoApp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 25)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 94.7),
+                  child: Icon(
+                    Icons.feedback_rounded,
+                    color: corPadraoApp,
+                    size: 55,
+                  ),
+                ),
+              ],
+            ), 
+             Divider(
+                  height: 0,
+                  thickness: 2.1,
+                  color: corPadraoApp,
+             ),
+                  
+          ],
+        ),
       ),
     );
   }
